4dc672b161c03bde736de083aaf844e31fd97a33
trabzi med amine <amine.trabzi@yahoo.com>
2021-12-09 11:38:33 +0000

    add code

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
+        Integer myInt = 5;
+        Integer yourInt = myInt * 2;
+        Integer result=100;
+	Integer firstNumber=55;
+	Integer secondNumber=5;
+        Integer result=firstNumber - secondNumber;
+
         System.out.println("Test Main.java with SonarQube");
     }
 }
