2bbfdb186d94b410f9204b6b5a96743ff7fe887c
trabzi med amine <amine.trabzi@yahoo.com>
2021-12-09 12:52:10 +0000

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+
+	public class MathService {
+        public int multiply(int firstNumber, int secondNumber) {
+
+        	return firstNumber * secondNumber;
+            }
+
+        public int subtract(int firstNumber, int secondNumber) {
+        	Integer myInt = null;
+        	Integer yourInt = myInt * 2;
+                return firstNumber - secondNumber;
+            }   
+        } 
+
+
         System.out.println("Test Main.java with SonarQube");
     }
 }
